98deec2c3c1ab089f9d9369d64cf67b5a3b59bb3
Lidii <lidiiany92@gmail.com>
2020-07-03 12:13:47 -0700

    refactored selectors in the tests

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -2,10 +2,27 @@ import { Selector } from 'testcafe';
 
 // prettier-ignore
 fixture `Getting started with TestCafe`
-    .page `https://devexpress.github.io/testcafe/example/`;
+    .page `https://devexpress.github.io/testcafe/example/`
+    .before(async t => {
+        //await runDatabaseReset()
+        //await seedTestData()
+    })
+    .beforeEach(async t => {
+        //Slowing speed of the test, 1 is default
+	    await t.setTestSpeed(1);
+    })
+    .after(async t => {
+        //Cleaning test data
+    })
+    .afterEach(async t => {
+        
+    })
 
 test('My first TestCafe test', async (t) => {
-	await t.typeText('#developer-name', 'John');
-	await t.click('#submit-button');
-	await t.expect(Selector('#article-header').innerText).contains('John');
+	const name_input = Selector('#developer-name');
+	const submit_button = Selector('#submit-button');
+	const articleText = Selector('#article-header').innerText;
+	await t.typeText(name_input, 'John');
+	await t.click(submit_button);
+	await t.expect(articleText).contains('John');
 });
