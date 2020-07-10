b7760cfcd40d1c8d65dee3238b58c94967a388e2
Lidii <lidiiany92@gmail.com>
2020-07-10 12:12:53 -0700

    login to application test (invalid case)

---
+++ tests/login.test.js
@@ -0,0 +1,25 @@
+import { Selector } from 'testcafe';
+
+// prettier-ignore
+fixture `Login Test`
+    .page `http://zero.webappsecurity.com/index.html`
+
+test('User cannot login with invalid credentials', async (t) => {
+	const signInButton = Selector('#signin_button');
+	await t.click(signInButton);
+
+	const loginForm = Selector('#login_form');
+	await t.expect(loginForm.exists).ok();
+
+	const usernameInput = Selector('#user_login');
+	const passwordInput = Selector('#user_password');
+
+	await t.typeText(usernameInput, 'invalid username', { paste: true });
+	await t.typeText(passwordInput, 'invalid password', { paste: true });
+
+	const submitButton = Selector('.btn-primary');
+	await t.click(submitButton);
+
+	const error = Selector('.alert-error').innerText;
+	await t.expect(error).contains('Login and/or password are wrong.');
+});
