90bbf5a62f391bbc761dacc1ade1eab21bdca164
Lidii <lidiiany92@gmail.com>
2020-07-17 12:15:35 -0700

    rafactored tests using navbar class

--- tests/forgottenPassword.test.js
+++ tests/forgottenPassword.test.js
@@ -1,4 +1,7 @@
 import { Selector } from 'testcafe';
+import Navbar from '../page-objects/components/Navbar';
+
+const navbar = new Navbar();
 
 // prettier-ignore
 fixture `Send Forgotten Password Test`
@@ -6,13 +9,12 @@ fixture `Send Forgotten Password Test`
 
 test('User can request new password to be sent to his email', async (t) => {
 	//Get selectors
-	const signInButton = Selector('#signin_button');
 	const linkToPassword = Selector('a').withText('Forgot your password ?');
 	const emailInput = Selector('#user_email');
 	const message = Selector('div').innerText;
 
 	//Actions
-	await t.click(signInButton);
+	await t.click(navbar.signInButton);
 	await t.click(linkToPassword);
 	await t.typeText(emailInput, 'test@gmail.com', { paste: true });
 	await t.pressKey('enter');
--- tests/search.test.js
+++ tests/search.test.js
@@ -1,5 +1,8 @@
 import { Selector } from 'testcafe';
 import xPathToCss from 'xpath-to-css';
+import Navbar from '../page-objects/components/Navbar';
+
+const navbar = new Navbar();
 
 // prettier-ignore
 fixture `Send Forgotten Password Test`
@@ -7,7 +10,6 @@ fixture `Send Forgotten Password Test`
 
 test('User can search for information using search box', async (t) => {
 	//Selectors
-	const searchBox = Selector('#searchTerm');
 	const resultsTitle = Selector('h2');
 	const linkText = Selector('div').innerText;
 
@@ -16,11 +18,11 @@ test('User can search for information using search box', async (t) => {
 	const css = xPathToCss(xPath);
 
 	//Actions
-	await t.typeText(searchBox, 'online bank', { paste: true });
+	await t.typeText(navbar.searchBox, 'online bank', { paste: true });
 	await t.pressKey('enter');
 
 	//Assertions
 	await t.expect(resultsTitle.exists).ok();
-	await t.expect(searchBox.exists).ok();
+	await t.expect(navbar.searchBox.exists).ok();
 	await t.expect(linkText).contains('Zero - Free Access to Online Banking');
 });
