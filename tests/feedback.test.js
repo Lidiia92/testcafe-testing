e99bb99cc9dac0b670ac1158d37f02f37d33ba1f
Lidii <lidiiany92@gmail.com>
2020-07-21 12:03:19 -0700

    refactored feedback test

--- tests/feedback.test.js
+++ tests/feedback.test.js
@@ -1,4 +1,7 @@
 import { Selector } from 'testcafe';
+import FeedbackPage from '../page-objects/pages/FeedbackPage';
+
+const feedbackPage = new FeedbackPage();
 
 // prettier-ignore
 fixture `Send Forgotten Password Test`
@@ -7,24 +10,19 @@ fixture `Send Forgotten Password Test`
 test('User can submit feedback via form', async (t) => {
 	//Selectors
 	const linkToFeedback = Selector('#feedback');
-	const form_name = Selector('#name');
-	const form_email = Selector('#email');
-	const form_subject = Selector('#subject');
-	const form_comment = Selector('#comment');
-	const form_submit = Selector('input').withAttribute(
-		'value',
-		'Send Message'
-	);
-	const message = Selector('div').innerText;
 
 	//Actions
 	await t.click(linkToFeedback);
-	await t.typeText(form_name, 'Test', { paste: true });
-	await t.typeText(form_email, 'test@gmail.com', { paste: true });
-	await t.typeText(form_subject, 'Test', { paste: true });
-	await t.typeText(form_comment, 'Test', { paste: true });
-	await t.click(form_submit);
+	await t.typeText(feedbackPage.form_name, 'Test', { paste: true });
+	await t.typeText(feedbackPage.form_email, 'test@gmail.com', {
+		paste: true,
+	});
+	await t.typeText(feedbackPage.form_subject, 'Test', { paste: true });
+	await t.typeText(feedbackPage.form_comment, 'Test', { paste: true });
+	await t.click(feedbackPage.form_submit);
 
 	//Assertions
-	await t.expect(message).contains('Thank you for your comments');
+	await t
+		.expect(feedbackPage.message.innerText)
+		.contains('Thank you for your comments');
 });
